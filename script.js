668fb8d6e108756c6d4c6be0b5a76a4084a9efde
Danillo Barcelos <danillofernandesb@gmail.com>
2023-05-19 14:40:22 -0300

    função descriptografar e copiar texto

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function criptografar(stringCriptografada) {
     let matrizChaves = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
     stringCriptografada = stringCriptografada.toLowerCase();
 
-    for (i = 0; i < matrizChaves.length; i++) {
+    for (let i in matrizChaves) {
         if (stringCriptografada.includes(matrizChaves[i][0])) {
             stringCriptografada = stringCriptografada.replaceAll(matrizChaves[i][0], matrizChaves[i][1])
         }
@@ -19,4 +19,36 @@ function btnCriptografar() {
     textArea.value = '';
 
     document.querySelector('.txt-alterado').style.backgroundImage = "none";
+}
+
+function descriptografar(stringDescriptografada) {
+    let matrizChaves = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDescriptografada = stringDescriptografada.toLowerCase();
+
+    for (let i in matrizChaves) {
+        if (stringDescriptografada.includes(matrizChaves[i][1])) {
+            stringDescriptografada = stringDescriptografada.replaceAll(matrizChaves[i][1], matrizChaves[i][0]);
+        }
+    }
+    return stringDescriptografada;
+}
+
+function btnDescriptografar() {
+    const textoDescriptografado = descriptografar(textArea.value);
+    textoAlterado.value = textoDescriptografado;
+    textArea.value = '';
+
+    document.querySelector('.txt-alterado').style.backgroundImage = "none";
+}
+
+function copiarTexto() {
+    textoAlterado.select();
+    return document.execCommand("copy");
+}
+
+function btnClicar(textoCopiado){
+    copiarTexto(textoAlterado.value);
+    textoAlterado.value = '';
+    document.querySelector('.txt-alterado').style.backgroundImage = "url(imagens/desenho1.png)";
+    
 }
